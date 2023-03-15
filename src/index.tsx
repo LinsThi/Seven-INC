0dac1b40e744b41cb4bb8af2b2abccf8ae5454d5
LinsThi <thiagolins13255@gmail.com>
2023-03-14 22:46:06 -0300

    feat: Adding StatusBar

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { Home } from '~/pages/Home';
 import theme from '~/styles/theme';
@@ -6,6 +7,7 @@ import theme from '~/styles/theme';
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar barStyle="light-content" backgroundColor="#292929" />
       <Home />
     </ThemeProvider>
   );
