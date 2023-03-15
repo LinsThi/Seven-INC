2c27d37bd57e36d05b086729e378b5ac548d372d
LinsThi <thiagolins13255@gmail.com>
2023-03-14 22:43:50 -0300

    chore: Adding new Colors

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,8 +1,14 @@
 import styled from 'styled-components/native';
+import { vs } from 'react-native-size-matters';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
   align-items: center;
   background-color: ${({ theme }) => theme.COLORS.MAIN};
+  padding-top: ${vs(60)}px;
+`;
+
+export const Content = styled.View`
+  width: 90%;
+  flex: 1;
 `;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,10 @@
 export default {
   COLORS: {
     MAIN: '#292929',
+    GRAY: '#505050',
+    LIGHT_GRAY: '#D3D3D3',
+    GREEN: '#44fe6e',
+    WHITE: '#FFFFFF',
   },
   FONTS: {},
 };
