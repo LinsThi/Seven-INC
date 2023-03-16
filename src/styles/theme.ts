41e1fc4121242925b304ba54125543880df311f1
LinsThi <thiagolins13255@gmail.com>
2023-03-16 12:16:08 -0300

    chore: Adding new colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -7,9 +7,11 @@ export default {
     WHITE: '#FFFFFF',
     RED: '#FF4040',
     BLACK: '#000000',
+    BLUE: '#2733a5',
 
     BG_BUTTON_CANCEL: '#9C9C9C',
     BG_BUTTON_CONFIRM: '#01ce2f',
+    COLOR_LABEL_INPUT: '#A0A0A0',
   },
   FONTS: {},
 };
