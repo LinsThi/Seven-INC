f773443982338029907c229cce5ad2f43ebe1427
LinsThi <thiagolins13255@gmail.com>
2023-03-16 10:14:24 -0300

    feat: Implementing EditEmployee Screen

--- src/pages/EditEmployee/index.tsx
+++ src/pages/EditEmployee/index.tsx
@@ -1,12 +1,101 @@
-import React from 'react';
-import { Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { useTheme } from 'styled-components/native';
+import { Button } from '~/components/Button';
+import { Input } from '~/components/Input';
+import { maskDate, maskCPF, maskPhoneNumber } from '~/utils/maskInput';
 
 import * as Sty from './styles';
 
 export function EditEmployee() {
+  const { COLORS } = useTheme();
+  const { goBack } = useNavigation();
+
+  const [name, setName] = useState('');
+  const [document, setDocument] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [birthDate, setBirthDate] = useState('');
+  const [salary, setSalary] = useState('');
+  const [createdAt, setCreatedAt] = useState('');
+
   return (
     <Sty.Container>
-      <Text>Hello</Text>
+      <Sty.ContainerHeader>
+        <Sty.Button onPress={() => goBack()}>
+          <Sty.Icon name="arrow-left" size={30} color={COLORS.WHITE} />
+        </Sty.Button>
+
+        <Sty.Title>Voltar</Sty.Title>
+      </Sty.ContainerHeader>
+
+      <Sty.Content>
+        <Sty.Form>
+          <Input
+            label="Nome"
+            isEmpty={!!name}
+            value={name}
+            onChangeText={setName}
+          />
+
+          <Input
+            label="CPF"
+            isEmpty={!!document}
+            value={maskCPF(document)}
+            onChangeText={setDocument}
+            keyboardType="number-pad"
+            maxLength={14}
+          />
+
+          <Input
+            label="E-mail"
+            isEmpty={!!email}
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+          />
+
+          <Input
+            label="Telefone"
+            isEmpty={!!phone}
+            value={maskPhoneNumber(phone)}
+            onChangeText={setPhone}
+            keyboardType="number-pad"
+            maxLength={16}
+          />
+
+          <Input
+            label="Nascimento"
+            isEmpty={!!birthDate}
+            value={maskDate(birthDate)}
+            onChangeText={setBirthDate}
+            keyboardType="number-pad"
+            maxLength={10}
+          />
+
+          <Sty.TwiceInputs>
+            <Input
+              label="Salário"
+              isEmpty={!!salary}
+              value={salary}
+              onChangeText={setSalary}
+              keyboardType="number-pad"
+              style={{ width: '80%' }}
+            />
+
+            <Input
+              label="Contratação"
+              isEmpty={!!createdAt}
+              value={maskDate(createdAt)}
+              onChangeText={setCreatedAt}
+              keyboardType="number-pad"
+              maxLength={10}
+            />
+          </Sty.TwiceInputs>
+
+          <Sty.ButtonConfirm title="Confirmar" titleColor="white" titleBold />
+        </Sty.Form>
+      </Sty.Content>
     </Sty.Container>
   );
 }
--- src/pages/EditEmployee/styles.ts
+++ src/pages/EditEmployee/styles.ts
@@ -1,7 +1,52 @@
+import { RFValue } from 'react-native-responsive-fontsize';
+import { ms, vs } from 'react-native-size-matters';
 import styled from 'styled-components/native';
+
+import MaterialComunityIcon, {
+  IconProps,
+} from '~/components/IconMaterialComunity';
+import { Button as Btn } from '~/components/Button';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
+  background-color: ${({ theme }) => theme.COLORS.MAIN};
 `;
+
+export const ContainerHeader = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: ${vs(10)}px ${ms(20)}px 0;
+`;
+
+export const Content = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  padding-top: ${vs(20)}px;
+`;
+
+export const Form = styled.View`
+  padding: 0 ${ms(30)}px;
+`;
+
+export const TwiceInputs = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(25)}px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.COLORS.WHITE};
+
+  margin-left: ${ms(10)}px;
+`;
+
+export const Button = styled.TouchableOpacity``;
+
+export const ButtonConfirm = styled(Btn)`
+  margin: ${vs(10)}px 0 ${vs(30)}px;
+`;
+
+export const Icon = styled(MaterialComunityIcon).attrs<IconProps>(
+  ({ name, color, size }) => ({ name, color: color, size: RFValue(size) }),
+)``;
