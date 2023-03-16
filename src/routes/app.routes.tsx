d7daf36e378ad4c9fdeaf60f24ee655c6057edcf
LinsThi <thiagolins13255@gmail.com>
2023-03-16 12:16:40 -0300

    feat: Adding SafeAreaView and refactor

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,28 +1,45 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { Platform, StatusBar, KeyboardAvoidingView } from 'react-native';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { ThemeProvider } from 'styled-components';
-import theme from '~/styles/theme';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { EditEmployee } from '~/pages/EditEmployee';
 import { Home } from '~/pages/Home';
-import { StatusBar } from 'react-native';
+
+import theme from '~/styles/theme';
 
 const { Navigator, Screen } = createStackNavigator();
 
+const navTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#292929',
+  },
+};
+
 export default function AppRoutes() {
   return (
-    <NavigationContainer>
-      <ThemeProvider theme={theme}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{ flex: 1 }}
+    >
+      <SafeAreaView style={{ flex: 1 }}>
         <StatusBar barStyle="light-content" backgroundColor="#292929" />
 
-        <Navigator
-          initialRouteName="Home"
-          screenOptions={{ headerShown: false }}
-        >
-          <Screen name="Home" component={Home} />
-          <Screen name="EditEmployee" component={EditEmployee} />
-        </Navigator>
-      </ThemeProvider>
-    </NavigationContainer>
+        <ThemeProvider theme={theme}>
+          <NavigationContainer theme={navTheme}>
+            <Navigator
+              initialRouteName="Home"
+              screenOptions={{ headerShown: false }}
+            >
+              <Screen name="Home" component={Home} />
+              <Screen name="EditEmployee" component={EditEmployee} />
+            </Navigator>
+          </NavigationContainer>
+        </ThemeProvider>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
