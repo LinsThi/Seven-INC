2b42f48064302a9aab1123ec6a3e74feda908b04
LinsThi <thiagolins13255@gmail.com>
2023-03-17 00:15:05 -0300

    feat: Adding new function utils maskInput

--- src/utils/maskInput.ts
+++ src/utils/maskInput.ts
@@ -21,3 +21,19 @@ export function maskDate(value: string) {
   newValue = newValue.replace(/^(\d{2})(\d{2})(\d{2})/g, '$1/$2/$3');
   return newValue;
 }
+
+export function verifyMask(value: string, mask: 'CPF' | 'phone' | 'date' | '') {
+  if (mask === 'CPF') {
+    return maskCPF(value);
+  }
+
+  if (mask === 'phone') {
+    return maskPhoneNumber(value);
+  }
+
+  if (mask === 'date') {
+    return maskDate(value);
+  }
+
+  return value;
+}
